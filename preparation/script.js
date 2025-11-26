badcd8402a4a9ec991b595a56c42589c3ee2666a
Pythia <el1vas@yahoo.gr>
2025-11-26 18:33:57 +0100

    Created element h1

--- preparation/script.js
+++ preparation/script.js
@@ -1,15 +1,16 @@
- const film = [
+ const film = 
   {
     title: "Killing of Flower Moon",
     director: "Martin Scorsese",
     times: ["15:35"],
     certificate: "15",
     duration: 112,
-  }
-];
+  };
 
 // Render a film card in the UI
 const filmCard = document.createElement("section");
-filmCard.textContent = film.title;
+const title = document.createElement("h1");
+title.textContent = film.title;
+filmCard.appendChild(title);
 document.body.appendChild(filmCard);
 
