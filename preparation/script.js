af4295d372fe29a3c25de074ab90eb638a06d47f
Pythia <el1vas@yahoo.gr>
2025-11-26 18:46:51 +0100

    Added template to html

--- preparation/script.js
+++ preparation/script.js
@@ -7,10 +7,14 @@
     duration: 112,
   };
 
-// Render a film card in the UI
+// Goal refactor this project to use film card template
 const filmCard = document.createElement("section");
 const title = document.createElement("h1");
+const director = document.createElement("p");
+const times = document.createElement("")
+director.textContent = film.director;
 title.textContent = film.title;
 filmCard.appendChild(title);
+filmCard.appendChild(director);
 document.body.appendChild(filmCard);
 
