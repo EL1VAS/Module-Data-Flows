89d6339fc9e370f7dc88ffa519dd5e3b0ced784d
Pythia <el1vas@yahoo.gr>
2025-12-04 13:20:59 +0100

    Created Index and script files added the js code from previous branch

---
+++ preparation/script.js
@@ -0,0 +1,35 @@
+const films = [
+  {
+    title: "Killing of Flower Moon",
+    director: "Martin Scorsese",
+    times: ["15:35"],
+    certificate: "15",
+    duration: 112,
+  },
+  {
+    title: "Typist Artist Pirate King",
+    director: "Carol Morley",
+    times: ["15:00", "20:00"],
+    certificate: "12A",
+    duration: 108,
+  }
+];
+
+// Goal refactor this project to use film card template
+function createFilmCard(films) {
+    const filmCard = document.getElementById("film-card-template").content.cloneNode(true);
+
+    filmCard.querySelector("h1").textContent = films.title;
+    filmCard.querySelector("p[data-director]").textContent = films.director;
+    filmCard.querySelector("time").textContent = films.times;
+    filmCard.querySelector("p[data-certificate]").textContent = films.certificate;
+    return filmCard;
+};
+
+function render() {
+  const filmCards = films.map(createFilmCard);
+  document.body.append(...filmCards);
+};
+
+render();
+
