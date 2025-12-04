fcbeea3964af6e793325acb3970f37f668b77d11
Pythia <el1vas@yahoo.gr>
2025-12-04 16:07:21 +0100

    Added a =

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -76,7 +76,7 @@ function render() {
     changeBut.className = "btn btn-success";
     wasReadCell.appendChild(changeBut);
     let readStatus = "";
-    if (myLibrary[i].check == false) {
+    if (myLibrary[i].check === false) { // Added a =
       readStatus = "Yes";
     } else {
       readStatus = "No";
