3368e958937d16b4a2a570575518f91250f81977
Pythia <el1vas@yahoo.gr>
2025-12-11 16:05:49 +0100

    Testing

--- numberfacts/script.js
+++ numberfacts/script.js
@@ -1,6 +1,12 @@
 let fact = document.getElementById("fact");
 let factText = document.getElementById("factText");
 let numberInput = document.getElementById("numberInput");
+
+console.log('numberInput:', numberInput);
+console.log('fact:', fact);
+console.log('factText:', factText);
+
+
 numberInput.addEventListener("input", getFactAjax);
 
 function getFactAjax() {
