b8eaade57911c1e4ee617af076519b0877dd6da5
Pythia <el1vas@yahoo.gr>
2025-12-11 19:59:11 +0100

    Coded along the video

--- numberfacts/script.js
+++ numberfacts/script.js
@@ -2,17 +2,32 @@ let fact = document.getElementById("fact");
 let factText = document.getElementById("factText");
 let numberInput = document.getElementById("numberInput");
 
-numberInput.addEventListener("input", getFactAjax);
+numberInput.addEventListener("input", getFactFetch);
 
 function getFactAjax() {
     let number = numberInput.value;
     let xhr = new XMLHttpRequest();
     xhr.open("GET", "http://numbersapi.com/"+number);
     xhr.onload = function() {
-        if (this.status == 200){
-            console.log(this.responseText);
+        if (this.status == 200 && number != ""){
+            fact.style.display = "block"
+            factText.innerText = this.responseText;
         }
 
     }
     xhr.send();
+}
+
+function getFactFetch() {
+    let number = numberInput.value;
+
+    fetch("http://numbersapi.com/"+number).then(response => response.text()).then(data => {
+        if (number != "") {
+            fact.style.display = "block"
+            factText.innerText = data;
+        }
+
+    })
+    .catch(error => console.log(error));
+
 }
