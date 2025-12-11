2a5130e97edbdc7b8f2f028f02a92098b148b31a
Pythia <el1vas@yahoo.gr>
2025-12-11 16:08:25 +0100

    Testing

--- numberfacts/script.js
+++ numberfacts/script.js
@@ -3,11 +3,14 @@ let factText = document.getElementById("factText");
 let numberInput = document.getElementById("numberInput");
 
 console.log('numberInput:', numberInput);
-console.log('fact:', fact);
-console.log('factText:', factText);
-
+console.log('About to add event listener');
 
 numberInput.addEventListener("input", getFactAjax);
+
+console.log('Event listener added');
+
+
+
 
 function getFactAjax() {
     console.log(123);
