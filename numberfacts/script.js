80960dae7e6e4fc7604f15100c624b83d5ae5b63
Pythia <el1vas@yahoo.gr>
2025-12-11 16:03:46 +0100

    Fixed the id names with ""

--- numberfacts/script.js
+++ numberfacts/script.js
@@ -1,6 +1,6 @@
-let fact = document.getElementById(fact);
-let factText = document.getElementById(factText);
-let numberInput = document.getElementById(numberInput);
+let fact = document.getElementById("fact");
+let factText = document.getElementById("factText");
+let numberInput = document.getElementById("numberInput");
 numberInput.addEventListener("input", getFactAjax);
 
 function getFactAjax() {
