057550af0d97b5244c740f18982a6bfa2dc71c36
Pythia <el1vas@yahoo.gr>
2025-12-12 21:37:18 +0100

    Added the title display in the second then of data and an image element to take the source and text from data. Append the img element to the image container to style after. Added the catch for errors and comments

--- fetch/programmer-humour/script.js
+++ fetch/programmer-humour/script.js
@@ -3,8 +3,17 @@ let image = document.getElementById("img-container");
 
 function getHumourFetch() {
     fetch(`https://xkcd.now.sh/?comic=latest`).then(response => response.json()).then(data => {
-        console.log(data);
-        }).catch(error => console.log(error))
+        console.log(data); // Logged in the console as reqested
+
+        title.innerText = data.safe_title; // Display the title from the data  
+
+        const imgElement = document.createElement("img"); // Creates an image element
+        imgElement.src = data.img; // Takes the image from data
+        imgElement.alt = data.alt; // Takes the text from data
+
+        image.appendChild(imgElement); // Appends the element to the container
+
+        }).catch(error => console.log(error)) // Error in the console
     };
 
 getHumourFetch();
