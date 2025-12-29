aa3e86475c758ecba70400a40b7dd78140ea9c10
Pythia <el1vas@yahoo.gr>
2025-12-29 13:46:50 +0100

    Moved script on top added defer, and added description of weather in js file

--- challenges/challenge-weather-app/script.js
+++ challenges/challenge-weather-app/script.js
@@ -1,14 +1,23 @@
 const WEATHER_API_KEY = "01c8510e47db5e342821cbfad960defc";
 
 function getWeather(city) {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${WEATHER_API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${WEATHER_API_KEY}`;
+
+    console.log('Fetching weather for:', city);
+    console.log('URL:', url);
 
     fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+    .then(function(response) {
+        return response.json;
     })
-    .catch(error => {
+    .then(function(data) {
+        console.log(data);
+
+        const conditionsElem = document.getElementById("consitions");
+        const description = data.weather[0].description;
+        conditionsElem.textContent = description;
+    })
+    .catch(function(error) {
         console.error("Error fetching weather...", error);
     })
     }
