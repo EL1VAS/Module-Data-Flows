25f3fd99c553e97d2ed4bd89fbfeac03fb06dcff
Pythia <el1vas@yahoo.gr>
2026-01-02 15:17:00 +0100

    Wrote the sales function

--- challenges/unit-testing/passing-tests/car-sales/car-sales.js
+++ challenges/unit-testing/passing-tests/car-sales/car-sales.js
@@ -1,3 +1,12 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  const total = {};
+
+  carsSold.forEach((car) => {
+    if (total[car.make]) {
+      total[car.make] += car.price;
+    }
+    total[car.make] = car.price;
+  });
+}
 
 module.exports = sales;
