c26fc27dfa68f2595a003b324d776f1718f5cd4e
Pythia <el1vas@yahoo.gr>
2026-01-02 15:51:55 +0100

    Wrote the test by creating a copy of the array

--- challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
+++ challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
@@ -2,8 +2,14 @@ let getLargestNumber = require("./largest-number");
 
 test("returns largest number in array", function () {
   // Arrange
+  const input = [3, 21, 88, 4, 36];
+  const copy = [...input];
+  const expected = 88;
   // Act
+  const result = getLargestNumber(input);
   // Assert
+  expect(result).toBe(expected);
+  expect(input).toEqual(copy);
 });
 
 // example
