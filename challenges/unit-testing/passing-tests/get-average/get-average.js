9cb47637dbdb4dac26d7beac5573b33fd136d23c
Pythia <el1vas@yahoo.gr>
2026-01-02 15:42:34 +0100

    Wrote the average function excluding non numerical strings

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,14 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  let numericValues = numbers.filter((item) => typeof item === "number");
+
+  let sum = 0;
+  for (i = 0; i < numericValues.length; i++) {
+    sum += numericValues[i];
+  }
+  return sum / numericValues.length;
+}
 
 module.exports = average;
